7da184459ec3658db013df42eb9cc9541eefedf9
Perez Githumbi <codakniggz145@gmail.com>
2023-12-05 13:52:59 +0300

    Started showing an error message when user attempts to access /login /register while authenticated

--- controllers/serverController.js
+++ controllers/serverController.js
@@ -4,8 +4,15 @@ const { namespace } = require("../namespace");
 const root = asyncHandler(async (req, res) => {
   const log = namespace.space("root");
   log.log("GET /");
+  log.log(req.user);
 
-  return res.render("index.ejs", { name: "Perez Githumbi" });
+  error_msg = req?.session?.args?.msg;
+  if (error_msg) req.session.args.msg = undefined;
+  return res.render("index.ejs", {
+    name: `${req.user[0].username}`,
+    email: req.user[0].email,
+    message: error_msg || "",
+  });
 });
 
 module.exports = { root };
